11370de8a60ce6f45d5b98b8618728ed28add656
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-12-06 16:15:03 -0300

    :sparkles: feat: criada primeira view de teste

--- app/views.py
+++ app/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from app.models import Escala
 
 def index(request):
-    return HttpResponse("Hello World")
+    todas_escalas = Escala.objects.all()
+    return HttpResponse(todas_escalas[1].id)
 
 # Create your views here.
