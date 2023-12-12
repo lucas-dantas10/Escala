23e21ba804e919d9670ebf113a9960235986e3e3
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-12-12 11:25:25 -0300

    :sparkles: feat : adicionado retorno de renderiação da pagina de HOME

--- app/views.py
+++ app/views.py
@@ -3,7 +3,9 @@ from django.http import HttpResponse
 from app.models import Escala
 
 def index(request):
-    todas_escalas = Escala.objects.all()
-    return HttpResponse(todas_escalas[1].id)
+    return render(request, "pages/home.html")
+
+    # todas_escalas = Escala.objects.all()
+    # return HttpResponse(todas_escalas[1].id)
 
 # Create your views here.
