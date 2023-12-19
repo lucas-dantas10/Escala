09e8fcd1963bff91704d0f5911590038a50800ca
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-12-19 18:16:43 -0300

    :sparkles: feat: configurações de urls

--- setup/settings.py
+++ setup/settings.py
@@ -137,3 +137,4 @@ STATIC_ROOT = BASE_DIR / "local-cdn" / "static"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_URL = 'users/login'
