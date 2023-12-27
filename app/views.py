4d718048ca930ef33af02f018985d67f245978d6
João <jlparceiro10@gmail.com>
2023-12-26 21:46:01 -0300

    Estilizando o index

--- app/views.py
+++ app/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 
+
 @login_required
 def index(request):
     return render(request, "pages/home.html")
+    
 
     # todas_escalas = Escala.objects.all()
     # return HttpResponse(todas_escalas[1].id)
