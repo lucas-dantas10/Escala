97adfe71df7d44b5c3b739a2bb136947ee9f83a4
João <jlparceiro10@gmail.com>
2024-01-04 14:46:34 -0300

    adicionando o botão para adm

--- app/views.py
+++ app/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 
-@login_required
 def index(request):
-    return render(request, "pages/home.html")
-    
+    usernames = User.objects.all()
+    return render(request, "pages/home.html", {"usernames": usernames})
 
     # todas_escalas = Escala.objects.all()
     # return HttpResponse(todas_escalas[1].id)
