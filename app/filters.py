c23041b0c7597617039ec74c0761a0ac292f214a
João <jlparceiro10@gmail.com>
2024-01-05 15:55:57 -0300

    adicionando filtro sem funcionar

--- app/filters.py
+++ app/filters.py
@@ -1,1 +1,8 @@
 import django_filters
+from django.contrib.auth.models import User
+
+
+class OrderFilter(django_filters.FilterSet):
+    class Meta:
+        model = User
+        fields = ['username']
--- app/views.py
+++ app/views.py
@@ -2,11 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from .filters import OrderFilter
 
 
 def index(request):
     usuarios = User.objects.all()
-    return render(request, "pages/home.html", {"usuarios": usuarios})
+
+    myFilter = OrderFilter()
+    context = {"usuarios": usuarios, "myFilter": myFilter}
+    return render(request, "pages/home.html", context)
 
     # todas_escalas = Escala.objects.all()
     # return HttpResponse(todas_escalas[1].id)
