6930257c84d57c52cecbe91f493945342d976f52
João <jlparceiro10@gmail.com>
2024-01-06 11:35:12 -0300

    Alterando o tipo de filtro

--- app/filters.py
+++ app/filters.py
@@ -3,6 +3,10 @@ from django.contrib.auth.models import User
 
 
 class OrderFilter(django_filters.FilterSet):
+    username = django_filters.CharFilter(
+        lookup_expr='startswith', label="Usuário"
+        )
+
     class Meta:
         model = User
         fields = ['username']
