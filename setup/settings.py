c65365551334bd09d5399154ffb8854a940443f0
Lucas Dantas Nogueira <lucas.dantas.nogueira@gmail.com>
2024-02-05 15:51:17 -0300

    feat: add comentario no final do arquivo

--- setup/settings.py
+++ setup/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',
-    'tailwind',
     'theme',
     'django_browser_reload',
     'django_filters',
@@ -87,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'church',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': '#Lucas10122003',
         'HOST': 'localhost',   # ou o endereço do seu servidor MySQL
         'PORT': '3306',        # porta padrão do MySQL
     }
@@ -141,18 +140,20 @@ STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
 
-STATIC_ROOT = BASE_DIR / "local-cdn" / "static"
+# STATIC_ROOT = BASE_DIR / "local-cdn" / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-LOGIN_URL = 'users/login'
+# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# LOGIN_URL = 'users/login'
 
-TAILWIND_APP_NAME = 'theme'
+# TAILWIND_APP_NAME = 'theme'
 
-INTERNAL_IPS = [
-    "127.0.0.1",
-]
+# INTERNAL_IPS = [
+#     "127.0.0.1",
+# ]
 
-NPM_BIN_PATH = "C:/Program Files/nodejs/npm.cmd"
+# NPM_BIN_PATH = "C:/Program Files/nodejs/npm.cmd"
+
+#oi
